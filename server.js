486d742d16c2d2f9bc56d0bf7cbe75d5cae8f238
Mike Erickson <codedungeon@gmail.com>
2016-09-08 14:00:04 -0700

    added leasot todo task

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
+
+'use strict'
+
 const express = require('express')
 const app     = express()
 const msg     = require('gulp-messenger')
 const chalk   = require('chalk')
-const fs      = require('fs')
 const Twig    = require('twig')
+const _       = require('lodash')
 
 var twig = Twig.twig;
 
-// // This section is optional and used to configure twig.
-// app.set("twig options", {
-//     strict_variables: false
-// });
+// This section is optional and used to configure twig.
+app.set('twig options', {
+    strict_variables: false
+});
 
 app.set('port', (process.env.PORT || 3000))
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'))  // static directory
 
-// default route to index.html
-// app.get('*', (req, res) => {
-//   res.write(fs.readFileSync(path.join(__dirname, 'public/index.html')));
-//   res.end();
-// });
-
+// Custom routes (not convered by static files, uses twig template engine)
 app.get('/', (req, res) => {
   res.render('index.twig', {
     firstName: 'Mike',
-    lastName: 'Erickson',
-    message : 'Hello'
+    lastName: 'Erickson'
   });
 });
 
 // lets startup this puppy
 app.listen(app.get('port'), () => {
-  console.log(chalk.cyan('Server Started ' + new Date()));
-  const serverInfo = chalk.yellow(`http://localhost:${app.get('port')}`)
   msg.log('\n')
-  msg.success('=', `               Application Running On: ${serverInfo}`, '=')
+  console.log(chalk.cyan('Server Started ' + new Date()));
+  msg.log('\n')
+  const serverInfo = chalk.yellow(`http://localhost:${app.get('port')}`);
+  msg.success('=', _.pad(`Application Running On: ${serverInfo}`, 80), '=')
 })
